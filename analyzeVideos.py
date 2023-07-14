a4c786e23e4aa5539d38e8d6708e4d6809ffc2ab
Mena Vhs <jgg1b@live.com.mx>
2023-07-14 14:50:10 -0600

    Datos globales en el main

--- analyzeVideos.py
+++ analyzeVideos.py
@@ -1,16 +1,25 @@
 import deeplabcut
+import main
 
-config_path = r'C:\Users\Angel\Desktop\EstimAI-Mena-2023-07-06\config.yaml'
-# video_path = r'C:\Users\Angel\Desktop\Prueba con ADFE\GrupoTCRata6Fase3PM2_R.mp4'
-video_path = r'D:\EstiAI_\4_JOINTS\0Iteration\1 SEC\ADFE_1-00-85_2-00-60 - 2SEC.mp4'
+config_path = main.config_path
+video_path = main.video_path
+
+shuffle_ = main.shuffle
+gputouse_ = main.gputouse
+videotype = main.videotype
+save_as_csv_ = main.save_as_csv_DeepLabCut
+destfolder_ = main.destfolder_DeepLabCut_documents
+
+deeplabcut.analyze_videos(config_path, video_path, videotype, shuffle=shuffle_, trainingsetindex=0, gputouse=gputouse_, save_as_csv=save_as_csv_, destfolder=destfolder_)
 
 
-# deeplabcut.analyze_videos(config_path, video_path, videotype='mp4', shuffle=1, trainingsetindex=0, gputouse=0, save_as_csv=True, destfolder=None)
-#
+# get_project_feature(joint, condition, unit, event_time)
+
+
 # deeplabcut.filterpredictions(config_path, video_path, shuffle=1, trainingsetindex=0, filtertype='arima', p_bound=0.01, ARdegree=3, MAdegree=1, alpha=0.01)
 # #
 # deeplabcut.plot_trajectories(config_path, video_path, videotype='mp4', shuffle=1, trainingsetindex=0, filtered=True , displayedbodyparts='all', showfigures=True)
 # #
 # deeplabcut.create_labeled_video(config_path, video_path, save_frames = True)
 # #
-deeplabcut.analyzeskeleton(config_path, video_path, videotype='mp4', shuffle=1, trainingsetindex=0, save_as_csv=True, destfolder=None)
+# deeplabcut.analyzeskeleton(config_path, video_path, videotype='mp4', shuffle=1, trainingsetindex=0, save_as_csv=True, destfolder=None)
---
+++ main.py
@@ -0,0 +1,17 @@
+
+joint = 'Spine'
+condition = 150
+unit = 'cm'  # or m
+event_time = 3  # sec
+
+config_path = r'C:\Users\Angel\Desktop\EstimAI-Mena-2023-07-06\config.yaml'
+video_path = r'D:\EstiAI_\4_JOINTS\0Iteration\1 SEC\ADFE_1-00-85_2-00-60 - 2SEC.mp4'
+# video_path = r'C:\Users\Angel\Desktop\PRUEBAS con videos externos\ADFE\Pruebas 100\100cm_recortado_brillo.mp4'
+
+shuffle = 1
+gputouse = 0
+videotype = 'mp4'
+save_as_csv_DeepLabCut = True
+destfolder_DeepLabCut_documents = None
+
+# destfolder_DeepLabCut_documents = r'D:\EstiAI_\4_JOINTS\0Iteration\1 SEC\Datos DLC'
