d3a03027011cf16f20ca696031305a0dd5b5b0b2
Mena Vhs <jgg1b@live.com.mx>
2023-07-22 09:33:20 -0600

    Redirection

--- Conditions/distance.py
+++ Conditions/distance.py
@@ -1,5 +1,5 @@
 from Conditions.conditions import distance_condition
-from writeInCSV import import_distance_contition_to_CSV
+from GenerateCSV.writeInCSV import import_distance_contition_to_CSV
 from timeCalculation import time_per_frame
 import main
 
