8c18f563a39406dc1bcf9629b5bc663f7ada008c
Jimena García <jgarcia.mca21@lania.edu.mx>
2023-07-24 12:24:23 -0600

    Generate directory for autosave centroids

--- PoseClassification/saveCentroind.py
+++ PoseClassification/saveCentroind.py
@@ -1,17 +1,21 @@
 import os
 
-
 def save_centroids_txt(centroids):
+    # Crear una carpeta por defecto llamada "Centroids" si no existe
+    folder_name = 'Centroids'
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
     # Crear un nombre de archivo único para los centroides
-    filename = 'centroides.txt'
+    filename = os.path.join(folder_name, 'centroides.txt')
     file_exists = os.path.exists(filename)
     n = 1
     while file_exists:
-        filename = f'centroids ({n}).txt'
+        filename = os.path.join(folder_name, f'centroides ({n}).txt')
         file_exists = os.path.exists(filename)
         n += 1
 
-    # Guardar los centroides en un archivo de texto
+    # Guardar los centroides en un archivo de texto en la carpeta "Centroids"
     with open(filename, 'w') as f:
         for centroid in centroids:
             f.write(f'{centroid}\n')
