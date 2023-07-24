2c51905384dbbeb8f3a345e234a3d7d59f8b0a0a
Jimena García <jgarcia.mca21@lania.edu.mx>
2023-07-24 15:32:52 -0600

    Save centroids in the same directory as the clusters

--- PoseClassification/saveCentroind.py
+++ PoseClassification/saveCentroind.py
@@ -1,17 +1,17 @@
 import os
 
-def save_centroids_txt(centroids):
-    # Crear una carpeta por defecto llamada "Centroids" si no existe
-    folder_name = 'Centroids'
+def save_centroids_txt(centroids, path):
+    # Crear una carpeta por defecto llamada "Centroids" si no existe en la ruta especificada
+    folder_name = os.path.join(os.path.dirname(path), 'Centroids')
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
     # Crear un nombre de archivo único para los centroides
-    filename = os.path.join(folder_name, 'centroides.txt')
+    filename = os.path.join(folder_name, 'centroids.txt')
     file_exists = os.path.exists(filename)
     n = 1
     while file_exists:
-        filename = os.path.join(folder_name, f'centroides ({n}).txt')
+        filename = os.path.join(folder_name, f'centroids ({n}).txt')
         file_exists = os.path.exists(filename)
         n += 1
 
