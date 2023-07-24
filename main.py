c48abff0dd84926a5b88bffb68c52a4532bf6340
Mena Vhs <jgg1b@live.com.mx>
2023-07-24 13:22:47 -0600

    Shoendfeld

--- main.py
+++ main.py
@@ -3,13 +3,13 @@
 
 # Distance conditions for experiment
 joint = 'Spine'
-condition = 100
+condition = 0
 unit = 'cm'  # or m
-event_time = 3  # sec
-video_path = r'C:\path\to\the\video\for\analyze\video.mp4'
+event_time = 0  # sec
+video_path = r'C:\Users\Angel\Desktop\PRUEBAS con videos externos\Shoenfeld\Grupo2r5s15_10fps_Recortado.mp4'
 
 # DeepLabCut data
-config_path = r'C:\path\to\DLC-project-2023-07-06\config.yaml'
+config_path = r'C:\Users\Angel\Desktop\EstimAI-Mena-2023-07-06\config.yaml'
 shuffle = 1
 gputouse = 0
 videotype = 'mp4'
@@ -18,11 +18,12 @@ destfolder_DeepLabCut_documents = None  # if "None", automatically DeepLabCut sa
                                         # Or you can write a path like: r'C:\path\...\directory'
 
 # Analyze a video with DeepLabCut
-analyze_videos = True
-filter_predictions = False
-plot_trajectories = False
-create_labeled_video = False
+analyze_videos = False
+filter_predictions = True
+plot_trajectories = True
 analyze_skeleton = False
+create_labeled_video = False
+
 
 if __name__ == "__main__":
     import exe
