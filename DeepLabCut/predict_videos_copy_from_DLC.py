cc53c192652cbb97281ebe9b0d87047d8f84b7bd
Lalo-gmz <angel-gomez@hotmail.com>
2023-09-01 10:26:22 -0600

    Path de la ruta original de predic_videos en DLC

--- DeepLabCut/predict_videos_copy_from_DLC.py
+++ DeepLabCut/predict_videos_copy_from_DLC.py
@@ -7,6 +7,9 @@
 # https://github.com/DeepLabCut/DeepLabCut/blob/master/AUTHORS
 #
 # Licensed under GNU Lesser General Public License v3.0
+
+# path of the file
+# C:\Users\Name\anaconda3\envs\DEEPLABCUT\Lib\site-packages\deeplabcut\pose_estimation_tensorflow\.py
 #
 
 
